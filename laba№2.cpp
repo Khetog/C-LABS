2d27bc2d28305263174ab7022686e66b1b47a22e
Khetog <88989350+Khetog@users.noreply.github.com>
2023-06-30 11:27:00 +0300

    Update laba№2.cpp

--- laba№2.cpp
+++ laba№2.cpp
@@ -1,43 +1,44 @@
-﻿///* Задача 27 (Жёлтая карта):
-//	Дан одномерный массив из N элементов. Вычислить количество элементов 
-//	массива, меньших среднего арифметического всех элементов.
-//*/
-//
-//#define _CRT_SECURE_NO_WARNINGS
-//#include <iostream>
-//
-//int main()
-//{
-//	int i, N, sum = 0,k=0;
-//	float arifm = 0;
-//	printf("Input size of array N: ");
-//	scanf("%d", &N);				// размерность массива
-//
-//
-//	while (N < 0 and N > 20) {
-//		printf("Vvedite razmer povtorno");
-//		scanf("%d", &N);
-//	}
-//
-//		
-//	int Arr[20];					// ввод данных массива
-//	printf("Input elements of array: ");
-//	for (i = 0; i < N; ++i) {
-//		scanf("%d", &Arr[i]);
-//		sum +=Arr[i];
-//	}
-//
-//
-//	arifm = sum / N;				// вычисление среднего арифметического
-//
-//
-//	i = 0;
-//	while (i < N) {					// подсчет кол-ва переменных, меньших, чем средн. арифм.
-//		if (Arr[i] < arifm) k++;
-//		i += 1;
-//	}
-//	printf("Result is: %d", k);
-//}
+/* Задача 27 (Жёлтая карта):
+	Дан одномерный массив из N элементов. Вычислить количество элементов 
+	массива, меньших среднего арифметического всех элементов.
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int i, N, sum = 0, k = 0;
+	float arifm = 0;
+	printf("Input size of array N: ");
+	scanf("%d", &N);				// размерность массива
+
+	while ((N < 1) or (N > 20)) {
+		printf("Input another size of array: ")
+		scanf("%d", &N);
+	}
+
+		
+	int Arr[20];					// ввод данных массива
+	printf("Input elements of array: ");
+	while (i < N) {
+		scanf("%d", &Arr[i]);
+		sum += Arr[i];
+		i++;
+	}
+
+	arifm = sum / N;				// вычисление среднего арифметического
+
+
+	i = 0;
+	while (i < N) {					// подсчет кол-ва переменных, меньших, чем средн. арифм.
+		if (Arr[i] < arifm) k++;
+		i += 1;
+	}
+	printf("Result is: %d", k);
+}
+
+
 
 
 //5-я лаба дан одномерный массив из N элементов. Вычислить количество элементов массива, меньших среднего арифметического всех элементов.
@@ -85,4 +86,34 @@ int main() {
 	pamparam(Arr, N, k, arif);
 
 	return 0;
+}
+
+
+//AAA,sdsa.asdAAA.sac,asc
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    printf("Input text string: ");
+    int ch;
+    int prevCh = ' '; // Предыдущий символ, начальное значение - пробел
+
+    while ((ch = getchar()) != '\n)') {
+        if ((ch != ',' and ch != '.' and ((ch < 65 and ch > 90) or (ch < 97 and ch > 122)))) {
+            printf("Input text again: ");
+        }
+        else {
+        }
+        if (ch == '.' && prevCh >= 'a' && prevCh <= 'z') {
+            prevCh -= 32;  // Преобразуем строчную букву перед точкой в заглавную
+        }
+        else if (ch == ',' && prevCh >= 'A' && prevCh <= 'Z') {
+            prevCh += 32;  // Преобразуем заглавную букву перед запятой в строчную
+        }
+        putchar(prevCh);
+        prevCh = ch; // Сохраняем текущий символ для последующей проверки
+    }
+
+    return 0;
 }
